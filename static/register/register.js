ec2fce7b0fc609e69467657551d3cd0d378572d1
Dmitriy <trueprogramdevelop@gmail.com>
2021-01-08 22:29:06 +0300

    simple create password on register page

--- static/register/register.js
+++ static/register/register.js
@@ -32,3 +32,32 @@ const GET_AUTH_URL = "http://0.0.0.0:5002/auth"
 function go_to_login_page() {
     window.location.replace(GET_AUTH_URL);
 }
+
+const GET_PASSWORD = "http://0.0.0.0:5002/v1/get_example_password"
+
+
+function get_example_password() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let json = JSON.parse(this.responseText);
+            // console.log(json);
+            // if (alert_ === true) {
+            //     alert("New settings deployed!");
+            // }
+            // plot(json)
+            // alert("Example password:\n" + json.example_password);
+            document.getElementById("example_password").innerHTML = "Example password: " + json.example_password;
+        }
+    };
+
+    xhr.open("GET", GET_PASSWORD, true);
+    // xhr.setRequestHeader("Content-Type", "application/json");
+    // let data = JSON.stringify(
+    //     {
+    //         "name": product_name
+    //     }
+    // );
+    xhr.send();
+}
